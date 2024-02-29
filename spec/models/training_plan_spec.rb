1e908ba67f3190b0d5512544e976a8496c0aeb1f
Philipe Leandro <philipe-leandro@hotmail.com>
2024-02-29 20:36:54 -0300

    Fix context name

--- spec/models/training_plan_spec.rb
+++ spec/models/training_plan_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TrainingPlan, type: :model do
-  context 'when user is valid' do
+  context 'when training_plan is valid' do
     let(:user) { create(:user) }
     let(:instance) { build(:training_plan, user: user) }
 
     it { expect(instance.valid?).to eq(true) }
   end
 
-  context 'when user is not valid' do
+  context 'when training_plan is not valid' do
     context 'when training_plan does have user' do
       let(:instance) { build(:training_plan) }
 
