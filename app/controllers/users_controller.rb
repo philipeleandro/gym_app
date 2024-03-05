0d8c37610ee9c98314554f65af4315c19af927f0
Philipe Leandro <philipe-leandro@hotmail.com>
2024-03-04 22:01:05 -0300

    Add endpoint new to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
