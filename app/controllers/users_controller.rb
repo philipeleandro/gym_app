1495da4400f6bf8f947244618c0c4881718a3889
Philipe Leandro <philipe-leandro@hotmail.com>
2024-03-05 21:48:26 -0300

    Use rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def index
     @users = User.all
@@ -19,6 +21,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name)
   end
