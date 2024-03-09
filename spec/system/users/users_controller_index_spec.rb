bfa73d03775dde960c258b1369db3d7b56cd53bb
Philipe Leandro <philipe-leandro@hotmail.com>
2024-03-09 11:28:11 -0300

    Adjust user tests

--- spec/system/users/users_controller_index_spec.rb
+++ spec/system/users/users_controller_index_spec.rb
@@ -4,25 +4,31 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :system do
   context 'when user visiter users list' do
+    let(:user) { create(:user) }
+
+    before do
+      user
+      visit users_path
+    end
+
+    context 'when has actions' do
+      it { expect(page).to have_content('Ação') }
+      it { expect(page).to have_content('Apagar') }
+      it { expect(page).to have_content('Fichas') }
+    end
+
     context 'when has registered users' do
-      let!(:user) { create(:user) }
-
-      it 'with success' do
-        visit users_path
-
-        expect(page).to have_content('Lista de usuários')
-        expect(page).to have_content('Nome')
-        expect(page).to have_content('Ação')
-        expect(page).to have_content(user.name)
-      end
+      it { expect(page).to have_content('Nome') }
+      it { expect(page).to have_content(user.name) }
     end
 
     context 'when does not have registered users' do
-      it 'returns a message' do
+      before do
+        user.delete
         visit users_path
-
-        expect(page).to have_content('Não há usuários cadastrados')
       end
+
+      it { expect(page).to have_content('Não há usuários cadastrados') }
     end
   end
 end
