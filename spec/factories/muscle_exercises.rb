4754b7a1beab764fe1177b69189b6cd8b382c9d5
Philipe Leandro <philipe-leandro@hotmail.com>
2024-03-16 10:03:27 -0300

    Adjust after remove column

--- spec/factories/muscle_exercises.rb
+++ spec/factories/muscle_exercises.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     repetition { 1 }
     set { 1 }
     observation { 'MyString' }
+    status_id { 1 }
     muscle { nil }
     exercise { nil }
   end
--- spec/factories/muscle_groups.rb
+++ spec/factories/muscle_groups.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :muscle_group do
     name { 'MyString' }
-    day { 'MyString' }
     training_plan { nil }
   end
 end
