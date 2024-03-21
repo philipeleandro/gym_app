f36241f25ff0282ee2af110779659a502077e58a
Philipe Leandro <philipe-leandro@hotmail.com>
2024-03-20 22:36:31 -0300

    Fix migration

--- db/migrate/20240316122947_remove_and_add_column_from_training_plan.rb
+++ db/migrate/20240316122947_remove_and_add_column_from_training_plan.rb
@@ -2,7 +2,6 @@
 
 class RemoveAndAddColumnFromTrainingPlan < ActiveRecord::Migration[7.1]
   def change
-    remove_column :training_plans, :day
     add_column :muscle_exercises, :status_id, :integer
   end
 end
