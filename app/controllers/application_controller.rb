91f8a1d4f2e4a61e6a002d3eb7695cfe617c57cc
Philipe Leandro <philipe-leandro@hotmail.com>
2024-03-23 21:55:51 -0300

    Create exercise helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   include MuscleGroupHelper
   include TrainingPlanHelper
   include ExerciseHelper
+  include MuscleExerciseHelper
 end
--- app/helpers/exercise_helper.rb
+++ app/helpers/exercise_helper.rb
@@ -4,4 +4,19 @@ module ExerciseHelper
   def exercices_by_muscle(muscle_id)
     Exercise.exercises_by_muscle(muscle_id)
   end
+
+  def separate_by_muscle
+    ordered_muscles = []
+
+    Exercise.muscle_ids.each do |muscle_name, muscle_id|
+      ordered_muscles << " -> #{Exercise.muscle_ids.key(muscle_id).upcase} "
+
+      Exercise.where(muscle_id: muscle_id).each do |exercise|
+        exercises_attributes = exercise.attributes.with_indifferent_access
+        ordered_muscles << [exercises_attributes[:name], exercises_attributes[:id]]
+      end
+    end
+
+    ordered_muscles
+  end
 end
