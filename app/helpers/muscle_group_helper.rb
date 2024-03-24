ab0e3cdd699c04f381c61ed0f0190835fde25ff6
Philipe Leandro <philipe-leandro@hotmail.com>
2024-03-24 18:09:48 -0300

    Create method to find muscle exercises

--- app/helpers/muscle_group_helper.rb
+++ app/helpers/muscle_group_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module MuscleGroupHelper
+  def find_exercises(muscle_group_id)
+    MuscleExercise.joins(:exercise_group)
+                  .where(exercise_group: { muscle_group_id: muscle_group_id })
+                  .order(:exercise_id)
+  end
 end
