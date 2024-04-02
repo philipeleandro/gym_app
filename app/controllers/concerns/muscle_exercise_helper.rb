d07e89c5a1a0162d6d3e4441440c43d92afa79bc
Philipe Leandro <philipe-leandro@hotmail.com>
2024-04-02 20:53:15 -0300

    Remove useless method

--- app/controllers/concerns/muscle_exercise_helper.rb
+++ app/controllers/concerns/muscle_exercise_helper.rb
@@ -6,8 +6,4 @@ module MuscleExerciseHelper
   def parse_params(params, key)
     params[key]
   end
-
-  def training_plan(muscle_group_id)
-    MuscleGroup.find(muscle_group_id).training_plan.id
-  end
 end
