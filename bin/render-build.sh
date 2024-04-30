f8b89f9f23bcf5203a831c99ef0671622dc6de8c
Philipe Leandro <philipe-leandro@hotmail.com>
2024-04-30 00:26:42 -0300

    Update render builder

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,11 @@
 # exit on error
 set -o errexit
 
+export PORT=1000
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:create
 bundle exec rails db:migrate
-bundle exec rails s -d -p 10000
+bundle exec rails s -d -p $PORT
